4fad2c59081f5d8203e2bfb8bb09cd1f37696f67
endritber <endritpb@gmail.com>
2024-02-22 13:29:38 -0500

    apache-airflow local environment

--- storage.py
+++ storage.py
@@ -3,8 +3,8 @@ import os
 from google.cloud import storage
 
 ROOT = '/Users/tin/fun/sdg-data-engineering'
-PATH_TO_CREDENTIALS = os.path.join(ROOT, 'secrets', 'service.json')
+CREDENTIALS = os.path.join(ROOT, 'secrets', 'service.json')
 
 if __name__ == '__main__':
-  client = storage.Client.from_service_account_json(PATH_TO_CREDENTIALS)
+  client = storage.Client.from_service_account_json(CREDENTIALS)
   print(client)
