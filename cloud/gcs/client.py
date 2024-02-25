5a4a8560bf23f89cbdd0ed106ab74991974189b0
endritber <endritpb@gmail.com>
2024-02-25 12:40:50 -0500

    refactor a bit

--- cloud/gcs/client.py
+++ cloud/gcs/client.py
@@ -1,6 +1,5 @@
 #!../../venv/bin/python
 import os
-import json
 from typing import Optional, TYPE_CHECKING, Union
 from path import GCSPath
 from pathlib import Path as PathLib
@@ -9,16 +8,9 @@ if TYPE_CHECKING:
   from google.auth.credentials import Credentials
 
 from google.cloud.storage import Client as StorageClient
-from google.api_core.exceptions import NotFound
 from google.auth.exceptions import DefaultCredentialsError
 from google.oauth2 import service_account
 
-
-def get_credentials():
-  credentials = service_account.Credentials.from_service_account_file(
-    os.path.join('/Users/tin/fun/dataengineering', 'secrets', 'google_service_secrets.json')
-  )
-  return credentials
 
 class Client:
   def __init__(
@@ -47,7 +39,7 @@ class Client:
   def _get_metadata(self, cloud_path: GCSPath):
     bucket = self.client.bucket(cloud_path.bucket)
     blob = bucket.blob(cloud_path.blob)
-    print(f'metadata on bucket: {bucket}, blob: {blob}')
+    print(f'LOGGING: metadata for bucket `{bucket}`, `blob: {blob}`')
 
     if blob is None:
       return None
@@ -57,21 +49,13 @@ class Client:
       "updated": blob.updated,
       "content_type": blob.content_type,
     }   
-
-  def _download_blob_todisk(self, cloud_path: GCSPath, disk_path: Union[str, os.PathLike]) -> PathLib:
-    bucket = self.client.bucket(cloud_path.bucket)
-    blob = bucket.get_blob(cloud_path.blob)
-    if disk_path: path_to_download = PathLib(disk_path)
-    else: path_to_download = PathLib(blob.split('/')[-1])
-    blob.download_to_filename(path_to_download)
-    return disk_path
   
   def _exists(self, cloud_path: GCSPath):
     if not cloud_path.blob:
       try:
         next(self.client.bucket(cloud_path.bucket).list_blobs())
         return True
-      except: return False
+      except: raise Exception(f"blob '{cloud_path.blob}' doesn't exist in bucket '{cloud_path.bucket}'")
       
     bucket = self.client.bucket(cloud_path.bucket)
     blob = bucket.blob(cloud_path.blob)
@@ -83,13 +67,19 @@ class Client:
       f = bucket.list_blobs(max_results=1, prefix=prefix)
       if bool(list(f)):
         return True
-      return False
+      raise Exception(f"blob '{cloud_path.blob}' doesn't exist in bucket '{cloud_path.bucket}'")
+    
+  def _download_blob_todisk(self, cloud_path: GCSPath, disk_path: Union[str, os.PathLike]) -> PathLib:
+    self._exists(cloud_path=cloud_path)
+    bucket = self.client.bucket(cloud_path.bucket)
+    blob = bucket.get_blob(cloud_path.blob)
+    if disk_path: path_to_download = PathLib(disk_path)
+    else: path_to_download = PathLib(blob.split('/')[-1])
+    blob.download_to_filename(path_to_download)
+    return disk_path
   
 if __name__ == '__main__':
-  credentials = get_credentials()
-  client = Client(credentials=credentials, project='sdg-data-engineering')
+  client = Client(credentials=service_account.Credentials.from_service_account_file('../secret/service-account.json'), project='sdg-data-engineering')
   cloud_path = GCSPath(cloud_path='gs://bucket-dropzone-example/input/train_base.csv')
-  print(client._get_metadata(cloud_path=cloud_path))
-
   # client._download_blob_todisk(cloud_path=cloud_path, disk_path='data.csv')
   print(client._exists(cloud_path=cloud_path))
