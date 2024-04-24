18a9d6d761719f7ac8990a013f7a60840690bd11
endritber <endritpb@gmail.com>
2024-04-24 16:59:29 -0400

    fixes

--- manifests/setup.sh
+++ manifests/setup.sh
@@ -5,17 +5,33 @@ if [ -z ${LOCAL_DAGS_PATH+x} ];
   else echo "LOCAL_DAGS_PATH is set to '$LOCAL_DAGS_PATH'";
 fi
 
+if [ -z ${LOCAL_DATA_PATH+x} ];
+  then echo "LOCAL_DATA_PATH is unset" && exit 1;
+  else echo "LOCAL_DATA_PATH is set to '$LOCAL_DATA_PATH'";
+fi
+
 handle_error() {
     echo "Error: $1"
 }
 
 if ! yq -i "
-.nodes[1].extraMounts[1].hostPath = \"$LOCAL_DAGS_PATH\" |
-.nodes[1].extraMounts[1].containerPath = \"/tmp/dags\"  |
-.nodes[2].extraMounts[1].hostPath = \"$LOCAL_DAGS_PATH\" |
-.nodes[2].extraMounts[1].containerPath = \"/tmp/dags\"  |
-.nodes[3].extraMounts[1].hostPath = \"$LOCAL_DAGS_PATH\" |
-.nodes[3].extraMounts[1].containerPath = \"/tmp/dags\"
+.nodes[1].extraMounts[0].hostPath = \"$LOCAL_DAGS_PATH\" |
+.nodes[1].extraMounts[0].containerPath = \"/tmp/dags\"  |
+.nodes[2].extraMounts[0].hostPath = \"$LOCAL_DAGS_PATH\" |
+.nodes[2].extraMounts[0].containerPath = \"/tmp/dags\"  |
+.nodes[3].extraMounts[0].hostPath = \"$LOCAL_DAGS_PATH\" |
+.nodes[3].extraMounts[0].containerPath = \"/tmp/dags\"
+" kind-cluster.yaml; then
+    handle_error
+fi
+
+if ! yq -i "
+.nodes[1].extraMounts[1].hostPath = \"$LOCAL_DATA_PATH\" |
+.nodes[1].extraMounts[1].containerPath = \"/tmp/data\"  |
+.nodes[2].extraMounts[1].hostPath = \"$LOCAL_DATA_PATH\" |
+.nodes[2].extraMounts[1].containerPath = \"/tmp/data\"  |
+.nodes[3].extraMounts[1].hostPath = \"$LOCAL_DATA_PATH\" |
+.nodes[3].extraMounts[1].containerPath = \"/tmp/data\"
 " kind-cluster.yaml; then
     handle_error
 fi
@@ -29,7 +45,7 @@ kubectl config set-context airflow --namespace=airflow
 kubectl -n airflow create secret generic my-webserver-secret --from-literal="webserver-secret-key=$(python3 -c 'import secrets; print(secrets.token_hex(16))')" || handle_error
 kubectl apply -f sc.yaml
 kubectl apply -f volumes.yaml
-helm repo add apache-airflow https://airflow.apache.org --set images.airflow.tag=2.2.3
+helm repo add apache-airflow https://airflow.apache.org 
 helm repo update
 helm search repo airflow
 helm upgrade --install airflow apache-airflow/airflow -n airflow -f values.yaml --debug
